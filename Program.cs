c28f5fab1629c2fcdc0e94f47c3fda75cc0aa75c
Gisele <giseleptbr@hotmail.com>
2025-09-02 15:18:24 -0300

    Implementação do desafio 1 em C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Entrada
+        Console.Write("Hora de início (hh mm): ");
+        string[] inicio = Console.ReadLine().Split(' ');
+        int horaInicio = int.Parse(inicio[0]);
+        int minutoInicio = int.Parse(inicio[1]);
+
+        Console.Write("Hora atual (hh mm): ");
+        string[] atual = Console.ReadLine().Split(' ');
+        int horaAtual = int.Parse(atual[0]);
+        int minutoAtual = int.Parse(atual[1]);
+
+        // Converte tudo para minutos
+        int inicioTotal = horaInicio * 60 + minutoInicio;
+        int atualTotal = horaAtual * 60 + minutoAtual;
+
+        // Calcula a duração
+        int duracao = atualTotal - inicioTotal;
+        int horas = duracao / 60;
+        int minutos = duracao % 60;
+
+        // Saída
+        Console.WriteLine($"Tempo total: {horas}h{minutos}min");
+    }
+}
+
